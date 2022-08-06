689a721cbf2f4f5df499630b89d48db46e1d8145
Logianer <derlohlgianer@gmail.com>
2022-08-06 18:38:22 +0200

    #1 Basic webserver finished

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
-const config = require('dotenv').config();
+require('dotenv').config();
 const app = express();
-app.listen(config.PORT);
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 app.use(express.static('static'));
+
+server.listen(process.env.PORT, () => {
+  console.log('listening on *:' + process.env.PORT);
+});
+
+io.on('connection', (socket) => {
+  console.log('new connection: ' + socket.id);
+});
